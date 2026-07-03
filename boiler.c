835ce79afcb535ff9d1e3bf01454ca7ac499c068
Otito Amuga <urphasin@gmail.com>
2026-07-02 20:11:04 -0600

    c

---
+++ boiler.c
@@ -0,0 +1,15 @@
+#include <stdio.h>    // printf, scanf
+#include <stdlib.h>   // malloc, qsort, abs
+#include <string.h>   // strlen, strcpy, memset
+#include <math.h>     // sqrt, pow, sin
+#include <limits.h>   // INT_MAX, INT_MIN
+#include <stdbool.h>  // bool, true, false
+#include <ctype.h>    // isdigit, isalpha
+
+int main() {
+
+  // Write below this line
+  
+  
+  return 0;
+}
--- main.h
+++
@@ -1,5 +0,0 @@
-void message1();
-void message2();
-void message3();
-void message4();
-#pragma once
