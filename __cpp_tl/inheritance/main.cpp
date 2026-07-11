215d2cbfc0edac744f9c0d69799e07e83553c8e3
Otito Amuga <urphasin@gmail.com>
2026-07-11 07:56:20 -0600

    d

---
+++ __cpp_tl/inheritance/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "include/playlist.hpp"
+
+
+int main() {
+
+  int x = add(5, 7);
+  std::cout << x << "\n";
+}
---
+++ __cpp_tl/inheritance/playlist.cpp
@@ -0,0 +1,5 @@
+#include "include/playlist.hpp"
+
+int add(int a, int b) {
+  return a + b;
+}
