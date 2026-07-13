0e62441a6f31df4e612ee5a8e47be47e030dbab5
Otito Amuga <urphasin@gmail.com>
2026-07-13 10:50:21 -0600

    b

--- __cpp_tl/inheritance/main.cpp
+++ __cpp_tl/inheritance/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include "include/playlist.hpp"
+using namespace std;
 
+
+void f(vector<double> d) {
+    for (auto x : d) {
+        double ans =  pow(x, 4) - 8*x*x + 1; 
+        cout << "f(" << x << ") = " << ans << ",\t";
+    }
+    cout << endl;
+}
+void m1(vector<double> d) {
+    for (auto x : d) {
+        double ans = 4*pow(x, 3) - 16*x;
+        cout << "f'(" << x << ") = " << ans << ",\t";
+    }
+    cout << endl;
+}
+void m2(vector<double> d) {
+    for (auto x : d) {
+        double ans = 12*x*x - 16;
+        cout << "f''(" << x << ") = " << ans << ",\t";
+    }
+    cout << endl;
+}
 
 int main() {
 
