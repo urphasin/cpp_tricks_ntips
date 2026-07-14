c6e4190f08271901704b1253c40c74d0a8e86850
Otito Amuga <urphasin@gmail.com>
2026-07-13 22:01:24 -0600

    v

--- __cpp_tl/inheritance/main.cpp
+++ __cpp_tl/inheritance/main.cpp
@@ -8,19 +8,19 @@ using namespace std;
 
 double f(double x)
 {
-  double ans = pow(x, 4) - 75;
+  double ans = 5*cos(x) - x - 1;
   return ans;
   // cout << "f(" << x << ") = " << ans << ",\t";
 }
 double m1(double x)
 {
-  double ans = 4*pow(x, 3);
+  double ans = -5*sin(x) - 1;
   return ans;
   // cout << "f'(" << x << ") = " << ans << ",\t";
 }
 double m2(double x)
 {
-    double ans = -2 * (cos(x) + pow(cos(x), 2) - pow(sin(x), 2));
+    double ans = 3*pow(x, 4) - 8*pow(x, 3) + 8;
     return ans;
     // cout << "f''(" << x << ") = " << ans << ",\t";
 }
@@ -28,8 +28,8 @@ void NewtonMethod()
 {
     std::cout << 
     std::fixed <<
-    std::setprecision(21);
-    double xn = 2.5;
+    std::setprecision(6);
+    double xn = 1.13;
     for (int i = 0; i < 10; i++)
     {
         if (std::abs(m1(xn)) < 1e-12)
@@ -49,5 +49,6 @@ int main() {
 
   int x = add(5, 7);
   std::cout << x << "\n";
+  cout << "f is : " << f(3) << endl;
   NewtonMethod();
 }
