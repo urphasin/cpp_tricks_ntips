0213e129754dd5d87db88b59695f469efe0c5b1e
Otito Amuga <urphasin@gmail.com>
2026-07-17 11:17:22 -0600

    f

--- classes/include/bin.hpp
+++ classes/include/bin.hpp
@@ -8,12 +8,17 @@
 class Bin {
   private:
     // This alias is safely scoped ONLY inside MyClass
-    template <typename T> using vector = std::vector<T>; 
+    template <typename T> using vector = std::vector<T>;
 
     vector<short> numbers;
     short count;
   public:
+    // constructor
     Bin(vector<short> trash1);
+
     // operator overloaded member function
     short operator++();
+    friend std::ostream& operator<<(
+      std::ostream& out, Bin& bin
+    );
 };
