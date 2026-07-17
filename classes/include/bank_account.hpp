89f192a377a9b4d807d0cc1ad412951a9e821707
Otito Amuga <urphasin@gmail.com>
2026-07-16 21:58:13 -0600

    f

---
+++ classes/include/bank_account.hpp
@@ -0,0 +1,26 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+
+#include <string>
+#include <iostream>
+
+class Account {
+  private:
+    std::string name;
+    long long balance;
+  public:
+    // constructors
+    Account(std::string name);
+
+    // accessors
+    const std::string& getName() const;
+    long long getBalance() const;
+
+    // mutators
+    void setName(std::string name);
+
+    // member functions
+    void deposit(int money);
+
+};
+#endif
--- classes/include/socks.hpp
+++ classes/include/socks.hpp
@@ -3,4 +3,6 @@
 
 #include <string>
 
+
+
 #endif
