d6516332f4139086037ac562ec5246c5e2347260
Otito Amuga <urphasin@gmail.com>
2026-07-17 11:00:27 -0600

    n

---
+++ classes/bin.cpp
@@ -0,0 +1,5 @@
+#include "include/bin.hpp"
+
+int Bin::operator++() {
+  return 
+}
---
+++ classes/include/bin.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+class Bin {
+  private:
+  // This alias is safely scoped ONLY inside MyClass
+    template <typename T> using vector = std::vector<T>; 
+
+    vector<short> numbers;
+    short count;
+  public:
+  
+  
+    Bin(vector<short> trash1);
+    // operator overloaded member function
+    short operator++();
+};
