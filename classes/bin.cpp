a78c980fd2a396ee95410b82e2c0c1dcd92de3ba
Otito Amuga <urphasin@gmail.com>
2026-07-17 11:27:06 -0600

    d

--- classes/bin.cpp
+++ classes/bin.cpp
@@ -1,6 +1,12 @@
 #include "include/bin.hpp"
 
+using std::ostream;
+using std::cout; // Perfectly legal at the top of a .cpp file
+
 short Bin::operator++() {
   return 0; 
 }
 
+ostream& operator<<(ostream& out, const Bin& bin) {
+
+}
--- classes/include/bin.hpp
+++ classes/include/bin.hpp
@@ -16,6 +16,12 @@ class Bin {
     // constructor
     Bin(vector<short> trash1);
 
+    // member functions
+    void printInfo() {
+      using std::cout; // Works perfectly here (inside function scope)
+      cout << "Printing bin details...\n";
+    }
+
     // operator overloaded member function
     short operator++();
     friend std::ostream& operator<<(
