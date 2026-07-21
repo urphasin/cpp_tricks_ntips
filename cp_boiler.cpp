655ee526f8ec38a45ff6621d5e31eae4ea18c64f
Otito Amuga <urphasin@gmail.com>
2026-07-21 13:25:15 -0600

    d

---
+++ cp_boiler.cpp
@@ -0,0 +1,240 @@
+#include <sstream>
+#include <iostream>
+// #include <type_traits> // c++20
+// #include <random>
+#include <cstring>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
+// #include <tuple>
+#include <stack>
+#include <queue>
+#include <deque>
+// #include <list>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+// #include <bit>
+#include <bitset>
+#include <limits>
+#include <numeric>
+#include <functional>
+#include <utility>
+#include <memory>
+using namespace std;
+
+/*
+POST SUBMISSION QUESTIONS:
+
+1.  
+
+*/
+
+
+// CONSTANTS
+const double pi = M_PI;
+const std::string endColor = "\x1b[0m";
+const std::string yellow = "\x1b[1;93m";
+
+
+// Default Classes and Structs
+struct Fraction {
+    long long num;
+    long long den;
+};
+
+// New Classes and Structs
+
+//##############################
+
+// Default Prototypes
+std::ostream& operator<<(std::ostream& os, unsigned __int128 x);
+std::ostream& operator<<(std::ostream& os, Fraction f);
+Fraction limit_denominator(double value, long long max_den = 1'000'000L);
+
+// New Prototypes
+
+
+//##############################
+
+// Default Template Definitions
+template <typename OP>
+void print_vector(vector<OP> arr) {
+    for (auto i : arr) {
+        std::cout << i << ", ";
+    }
+    std::cout<<std::endl;
+    for (size_t i{0}; i < arr.size(); i++)
+    {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename OP>
+void print_array(int size, OP *arr) {
+    for(int i = 0; i < size; i++) {
+        std::cout << arr[size] << ", ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename O, typename P>
+void print_unordered_map(std::unordered_map<O, P> table) {
+    for(auto i = table.begin(); i != table.end(); i++) {
+        std::cout << i->first << " : " << i->second << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+// New Templatate Definitions
+
+
+//##############################
+
+
+// Write below this line.
+long long nCr(double n, double r) {
+  if (r > n || r < 0 || n < 0) {
+    return 0;
+  }
+
+  long long result = 1;
+  for (int i = 0, j = r; i < r; i++, j--) {
+    result *= (n - i) / j;
+  }
+  return result;
+}
+
+double binomial(double p, double n, double r) {
+  double ans = nCr(n, r) * pow(p, r) * pow((1 - p), n-r);
+  return ans;
+}
+double f(double x)
+{
+  double ans = cos(pow(x, 2) - x) - pow(x, 4);
+  return ans;
+  // cout << "f(" << x << ") = " << ans << ",\t";
+}
+double m1(double x)
+{
+  double ans = -sin(pow(x, 2) - x) * (2 * x - 1) - 4 * pow(x, 3);
+  return ans;
+  // cout << "f'(" << x << ") = " << ans << ",\t";
+}
+double m2(double x)
+{
+    double ans = -2 * (cos(x) + pow(cos(x), 2) - pow(sin(x), 2));
+    return ans;
+    // cout << "f''(" << x << ") = " << ans << ",\t";
+}
+
+void NewtonMethod()
+{
+    std::cout << 
+    std::fixed <<
+    std::setprecision(21);
+    double xn = -0.5;
+    for (int i = 0; i < 10; i++)
+    {
+        if (std::abs(m1(xn)) < 1e-12)
+        {
+            std::cout << "Derivative too small\n";
+            return;
+        }
+        xn = xn - (f(xn) / m1(xn));
+        cout << xn << ", ";
+    }
+    cout << std::setprecision(6) << endl;
+}
+
+
+
+// solve
+void solve() {
+    
+}
+
+
+//##############################
+
+// MAIN
+int main() {
+    /*
+    unsigned __int128 is a compiler extension (mainly GCC/Clang), 
+    not a standard C++ integer type, 
+    so the standard stream operators (<<) were never overloaded for it.
+    */
+    unsigned __int128 x = 4;
+    // std::cout << x << std::endl;
+    int arr[] = {1, 9, 7, 3, 2, 5};
+    *(arr + 2) = 6;
+    cout << *(arr + 2) << endl;
+
+    double d = 0.3333333333333;
+    Fraction f = limit_denominator(d);
+    cout << f << endl;
+
+    // Write below this line
+
+
+    return 0;
+}
+
+//##############################
+
+// Default Definitions
+std::ostream& operator<<(std::ostream& os, unsigned __int128 x) {
+    
+    if (x == 0) {
+        os << '0';
+        return os;
+    }
+
+    std::string s;
+
+    while (x > 0) {
+        s += '0' + (x % 10);
+        x /= 10;
+    }
+
+    std::reverse(s.begin(), s.end());
+
+    os << s;
+    return os;
+}
+std::ostream& operator<<(std::ostream& os, Fraction f) {
+    os << f.num << "/" << f.den << std::endl;
+    return os;
+}
+Fraction limit_denominator(double value, long long max_den) {
+    long long a = std::floor(value);
+
+    long long h1 = 1, k1 = 0;
+    long long h = a, k = 1;
+
+    double frac = value;
+
+    while ( std::fabs(value - static_cast<double>(h) / k) > 1e-12 && k < max_den ) {
+        frac = 1.0 / (frac - a);
+        a = std::floor(frac);
+
+        long long h2 = h1;
+        h1 = h;
+
+        long long k2 = k1;
+        k1 = k;
+
+        h = a * h1 + h2;
+        k = a * k1 + k2;
+    }
+
+    return {h, k};
+}
+
+// New Definitions
+
