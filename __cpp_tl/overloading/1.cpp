74c6e5e970bcc40d8f2cb30d6a20b30042b45d8c
Otito Amuga <urphasin@gmail.com>
2026-07-21 13:27:53 -0600

    h

---
+++ __cpp_tl/overloading/1.cpp
@@ -0,0 +1,58 @@
+#include <sstream>
+#include <iostream>
+// #include <type_traits> // c++20
+// #include <random>
+#include <cstring>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
+// #include <tuple>
+#include <stack>
+#include <queue>
+#include <deque>
+// #include <list>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+// #include <bit>
+#include <bitset>
+#include <limits>
+#include <numeric>
+#include <functional>
+#include <utility>
+#include <memory>
+using namespace std;
+
+
+// CONSTANTS
+const double pi = M_PI;
+const std::string endColor = "\x1b[0m";
+const std::string yellow = "\x1b[1;93m";
+
+
+// Default Classes and Structs
+
+
+// solve
+void solve() {
+    
+}
+
+
+//##############################
+
+// MAIN
+int main() {
+  // Write below this line
+  
+  
+  
+  return 0;
+}
+
+
+
