8cf4e81612630d5264ea9415fd35b46d93f8ef15
Otito Amuga <urphasin@gmail.com>
2026-07-28 00:10:26 -0600

    d

---
+++ __cpp_tl/5.cpp
@@ -0,0 +1,71 @@
+#include <sstream>
+#include <iostream>
+// #include <type_traits> // c++20
+// #include <random>
+#include <cstring>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
+// #include <tuple>
+#include <stack>
+#include <queue>
+#include <deque>
+// #include <list>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+// #include <bit>
+#include <bitset>
+#include <limits>
+#include <numeric>
+#include <functional>
+#include <utility>
+#include <memory>
+using namespace std;
+
+
+// CONSTANTS
+const double pi = M_PI;
+const std::string endColor = "\x1b[0m";
+const std::string yellow = "\x1b[1;93m";
+
+
+class A {
+  public:
+    virtual void add() {
+      cout << "Added" << endl;
+    }
+};
+
+class B {
+  public:
+    virtual void call() {
+      cout << "Called" << endl;
+    }
+};
+
+class C : public A, public B {
+  
+};
+
+
+
+//##############################
+
+// MAIN
+int main() {
+  // Write below this line
+  C c;
+  c.call();
+  
+  
+  return 0;
+}
+
+// g++ 5.cpp -o ans && ./ans && rm -rf ./ans 
+
+
