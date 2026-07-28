dbe03ff2e65ef561e46a6f8698bc62b5f8f94b8a
Otito Amuga <urphasin@gmail.com>
2026-07-27 21:34:13 -0600

    d

---
+++ __cpp_tl/4.cpp
@@ -0,0 +1,68 @@
+#include <sstream>
+#include <iostream>
+// #include <type_traits> // c++20
+// #include <random>
+#include <cstring>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
+// #include <tuple>
+#include <stack>
+#include <queue>
+#include <deque>
+// #include <list>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+// #include <bit>
+#include <bitset>
+#include <limits>
+#include <numeric>
+#include <functional>
+#include <utility>
+#include <memory>
+using namespace std;
+
+
+// CONSTANTS
+const double pi = M_PI;
+const std::string endColor = "\x1b[0m";
+const std::string yellow = "\x1b[1;93m";
+
+
+class DistanceClass {
+  private:
+    int feet;
+    int inches;
+
+  public:
+    void show() {
+      cout << feet << "' " << inches << "\"" << endl;
+    }
+};
+
+
+// solve
+void solve() {
+    
+}
+
+
+//##############################
+
+// MAIN
+int main() {
+  // Write below this line
+  
+  
+  
+  return 0;
+}
+
+// g++ 0.cpp -o ans && ./ans && rm -rf ./ans 
+
+
