4bb6cdd7dbc1efabfd656561d9aa0848322a968c
Otito Amuga <urphasin@gmail.com>
2026-07-31 17:54:01 -0600

    d

--- __cpp_tl/main.cpp
+++ __cpp_tl/main.cpp
@@ -1,12 +1,8 @@
-#include "./include/vector2d.hpp"
+#include <iostream>
 
 int main() {
   
-  Vector2D v1{5, 2};
-  Vector2D v2{10, 15};
-
-  Vector2D v3 = v1 + v2 + v1;
-  v3.print();
+  
 
   return 0;
 }
