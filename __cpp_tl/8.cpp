3cf49290a8ba11d0b91416c8cc57aa3f5da1bb17
Otito Amuga <urphasin@gmail.com>
2026-08-02 00:02:07 -0600

    x

---
+++ __cpp_tl/8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class DivisionByZero : public runtime_error {
+public:
+  DivisionByZero() : runtime_error("divide by zero") {}
+};
+
+double divide(double number, double by) {
+  if (by == 0) {
+    throw DivisionByZero();
+  }
+
+  return number / by;
+}
+
+int main() {
+  try {
+    cout << divide(5, 0) << endl;
+  } catch(DivisionByZero d) {
+    cerr << d.what() << endl;
+  }
+}
