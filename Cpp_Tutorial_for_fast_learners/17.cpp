07bb153f61bc9e7b9fb3408211da07fe5f2d6fcf
Otito Amuga <urphasin@gmail.com>
2026-08-02 17:59:31 -0600

    s

--- Cpp_Tutorial_for_fast_learners/17.cpp
+++ Cpp_Tutorial_for_fast_learners/17.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
 template<typename E>
 class Array {
-  
+public:
+  Array(int n) : size(n) {
+    data = new E[size];
+  }
+  ~Array() {
+    delete data;
+  }
+
+  // overload functions
+  void operator[]() {}
+
+private:
+  E* data;
+  int size;
 };
+
+int main() {
+  Array<int>numbers(5);
+}
