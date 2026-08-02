d2d21f546ecc2afacc403011616a5505058ad752
Otito Amuga <urphasin@gmail.com>
2026-08-02 14:06:19 -0600

    f

--- Cpp_Tutorial_for_fast_learners/16.cpp
+++ Cpp_Tutorial_for_fast_learners/16.cpp
@@ -6,5 +6,16 @@ using namespace std;
 
 template<typename K, typename V>
 class Pair {
-  
+public:
+  Pair(K k, V v) : key(k), value(v) {}
+  ~Pair() {}
+
+  K getKey() const { return key; }
+  V getValue() const { return value; }
+
+  void setKey(K key) : key(key) {}
+  void setValue(V value) : value(value) {}
+private:
+  K key;
+  V value;
 };
