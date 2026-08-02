4f3d1ee96244e659cbdc0a5b23ef87c92a77a6bd
Otito Amuga <urphasin@gmail.com>
2026-08-02 14:25:29 -0600

    s

--- Cpp_Tutorial_for_fast_learners/16.cpp
+++ Cpp_Tutorial_for_fast_learners/16.cpp
@@ -13,8 +13,12 @@ public:
   K getKey() const { return key; }
   V getValue() const { return value; }
 
-  void setKey(K key) : key(key) {}
-  void setValue(V value) : value(value) {}
+  void setKey(K key) {
+    this->key = key;
+  }
+  void setValue(V value) {
+    this->value = value;
+  }
 private:
   K key;
   V value;
@@ -22,8 +26,28 @@ private:
 
 int main() {
 
+  // 2 stack allocation
   Pair<int, int> pair1(5, 6);
   pair1.setKey(20);
   cout << pair1.getKey() << "  :  " << pair1.getValue() << "\n\n";
+
+  Pair<int, short> pair2(15, 16);
+  pair2.setValue(2001);
+  cout << pair2.getKey() << "  :  " << pair2.getValue() << "\n\n";  
   
+
+  //new dynamic allocation 
+  Pair<double, long>* pair3 = new Pair(5.0, 20L);
+  pair3->setKey(45.8);
+  pair3->setValue(156L);
+  cout << pair3->getKey() << "  :  " << pair3->getValue() << "\n\n";
+
+  delete pair3;
+
+
+  // smart pointer heap allocation
+  unique_ptr<Pair<string, char>> pair4 = make_unique<Pair<string, char>>("Otito", 'c');
+  cout << pair4->getKey() << "  :  " << pair4->getValue() << "\n\n";  
+
+  return 0
 }
